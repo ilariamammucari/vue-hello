3961fdfcd4a9d3418babb18672238494873b3759
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-25 13:54:02 +0100

    stampo un messaggio con Vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert();
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo. Poi aggiungete all'immagine una classe sempre presa dal data.
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        messaggio: 'Ciao da Vue.js'
+    }
+});
